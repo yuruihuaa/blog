4451255ce43704293826885376ee6df7f35129ed
yuruihuaa <yuruihuaa@outlook.com>
2021-05-13 18:27:25 +0800

    Use vuepress blog theme

--- blog/.vuepress/config.js
+++ blog/.vuepress/config.js
@@ -1,4 +1,39 @@
 module.exports = {
-    title: 'Hello VuePress',
-    description: 'Just playing around'
+  title: 'Yu Blog',
+  theme: '@vuepress/theme-blog',
+  themeConfig: {
+    smoothScroll: true,
+    nav: [
+      {
+        text: 'Blog',
+        link: '/',
+      },
+      {
+        text: 'Tags',
+        link: '/tag/',
+      },
+    ],
+    footer: {
+      contact: [
+        {
+          type: 'github',
+          link: 'https://github.com/yuruihuaa',
+        },
+        {
+          type: 'video',
+          link: 'https://space.bilibili.com/322365128',
+        },
+      ],
+      copyright: [
+        {
+          text: 'VuePress',
+          link: 'https://github.com/vuejs/vuepress',
+        },
+        {
+          text: 'Blog Theme',
+          link: 'https://github.com/vuepress/vuepress-theme-blog',
+        },
+      ],
+    },
+  }
 }
